23e93942d20121a8b63e1e99b313516613c3b3f3
Nakul <nakula@gmail.com>
2014-04-13 02:47:02 +0530

    more refactoring

--- tests/test.rb
+++ tests/test.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 
 RUNNER_DIR = Dir.pwd.gsub(/\/tests$/,"") + "/"
+
 def execute(cmd)
   `#{cmd}`
 end
@@ -31,55 +32,34 @@ def runBg(cmd, linestomatch, genre, test_strings)
 end
 
 class Test
-  def run_project
+  def run_project(reponame, test_framework, passed_array, browser_url)
+    repo_path = "#{RUNNER_DIR}tests/external-repos/"
+    conf_path = "#{RUNNER_DIR}tests/conf/"
+
+    execute("cd #{repo_path}#{reponame} && npm install")
+    execute("cd #{repo_path}#{reponame} && cp #{conf_path}#{reponame}.json #{repo_path}#{reponame}/browserstack.json")
+    pid = runBg("cd #{repo_path}#{reponame} && ../../../bin/cli.js", 1, "#{test_framework}-#{reponame}", passed_array)
+    execute("open http://localhost:8888/#{browser_url}")
+    Process.wait(pid)
+    sleep(1)
   end
   
   def qunit
-    repo_path = "#{RUNNER_DIR}tests/external-repos/"
-    conf_path = "#{RUNNER_DIR}tests/conf/"
-    execute("cd #{repo_path}underscorejs && npm install")
-    execute("cd #{repo_path}underscorejs && cp #{conf_path}underscorejs.json #{repo_path}underscorejs/browserstack.json")
-    pid = runBg("cd #{repo_path}underscorejs && ../../../bin/cli.js", 1, "qunit-underscorejs", ["631 of 631 passed"])
-    execute("open http://localhost:8888/test/index.html")
-    Process.wait(pid)
-    sleep(1)
-
-    execute("cd #{repo_path}Modernizr && npm install")
-    execute("cd #{repo_path}Modernizr && cp #{conf_path}Modernizr.json #{repo_path}Modernizr/browserstack.json")
-    pid2 = runBg("cd #{repo_path}Modernizr && ../../../bin/cli.js", 1, "qunit-Modernizr", ["20 of 41 passed"])
-    execute("open http://localhost:8888/test/index.html")
-    Process.wait(pid2)
-
+    run_project("underscorejs", "qunit", ["631 of 631 passed"], "test/index.html")
+    run_project("Modernizr", "qunit", ["20 of 41 passed"], "test/index.html")
     # https://github.com/bitovi/funcunit/
     # https://github.com/twbs/bootstrap
   end
 
   def mocha
-    repo_path = "#{RUNNER_DIR}tests/external-repos/"
-    conf_path = "#{RUNNER_DIR}tests/conf/"
-    
-    execute("cd #{repo_path}url.js && npm install")
-    execute("cd #{repo_path}url.js && cp #{conf_path}url.js.json #{repo_path}url.js/browserstack.json")
-    pid = runBg("cd #{repo_path}url.js && ../../../bin/cli.js", 1, "mocha-urlJs", ["35 of 35 passed"])
-    execute("open http://localhost:8888/test.html")
-    Process.wait(pid)
-    sleep(1)
-    
+    run_project("url.js", "mocha", ["35 of 35 passed"], "test.html")    
     # https://github.com/browserstack/microjungle
     # https://github.com/dhimil/mocha
     # https://github.com/auth0/auth0.js
   end
 
   def jasmine
-    repo_path = "#{RUNNER_DIR}tests/external-repos/"
-    conf_path = "#{RUNNER_DIR}tests/conf/"
-    
-    execute("cd #{repo_path}mout && npm install")
-    execute("cd #{repo_path}mout && cp #{conf_path}mout.json #{repo_path}mout/browserstack.json")
-    pid = runBg("cd #{repo_path}mout && ../../../bin/cli.js", 1, "jasmine-mout", ["978 of 978 passed"])
-    execute("open http://localhost:8888/tests/runner.html")
-    Process.wait(pid)
-    sleep(1)
+    run_project("mout", "jasmine", ["978 of 978 passed"], "tests/runner.html")
   end
 
   def run
