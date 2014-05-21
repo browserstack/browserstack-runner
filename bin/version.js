995149cf8b86992aacf3bc85e649cedc8ac72984
Dhimil Gosalia <dhimil@gmail.com>
2014-05-21 17:23:08 +0530

    more minor changes

--- bin/version.js
+++ bin/version.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
 
-packagePath = require('path').resolve(__dirname, "../package.json");
-packageJson = require(packagePath);
-console.log("BrowserstackRunner @", packageJson["version"]);
+var packagePath = require('path').resolve(__dirname, "../package.json"),
+    packageJson = require(packagePath);
+console.log("browserstack-runner @", packageJson["version"]);
